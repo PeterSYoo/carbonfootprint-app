895578bc5559b8712f15c778bd455986ad13b43c
Peter Yoo <trustintofu@gmail.com>
2022-11-16 19:01:09 -0800

    feat: Wraps providers to app.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,28 @@
 import { ThemeProvider } from 'next-themes';
 import { Header } from '../components/Header.components';
+import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import {
+  QueryClientProvider,
+  QueryClient,
+  Hydrate,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider enableSystem={true} attribute="class">
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <SessionProvider session={pageProps.session}>
+            <ThemeProvider enableSystem={true} attribute="class">
+              <Header />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </SessionProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
