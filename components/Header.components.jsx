c06537404746c71444c0908b039ca4131985d9a1
Peter Yoo <trustintofu@gmail.com>
2022-11-16 18:59:40 -0800

    feat: Added conditional to render content based on user login.

--- components/Header.components.jsx
+++ components/Header.components.jsx
@@ -1,16 +1,44 @@
+import { useSession, signOut } from 'next-auth/react';
+import Link from 'next/link';
 import React from 'react';
 import { ThemeButton } from './ThemeButton.components';
 
-export const Header = () => {
+const Guest = () => {
   return (
     <>
       <div className="flex justify-between mt-6 mx-auto px-10 md:max-w-[1024px]">
-        <div className="text-xl font-bold">Logo</div>
+        <Link href="/">
+          <button className="text-xl font-bold">Logo</button>
+        </Link>
         <div className="flex items-center gap-10">
           <ThemeButton />
-          <button>Signin</button>
+          <Link href="/login">
+            <button>Login</button>
+          </Link>
         </div>
       </div>
     </>
   );
+};
+
+const User = () => {
+  return (
+    <>
+      <div className="flex justify-between mt-6 mx-auto px-10 md:max-w-[1024px]">
+        <Link href="/">
+          <button className="text-xl font-bold">Logo</button>
+        </Link>
+        <div className="flex items-center gap-10">
+          <ThemeButton />
+          <button onClick={() => signOut()}>Logout</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const Header = () => {
+  const { data: session } = useSession();
+
+  return <>{session ? User({ session }) : Guest()}</>;
 };
