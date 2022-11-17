c647c050820347407e8aa136f551112c97bce5d9
Peter Yoo <trustintofu@gmail.com>
2022-11-16 19:04:09 -0800

    feat: Conditionally render user when logged in.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { useSession } from 'next-auth/react';
+
+const Guest = () => {
   return (
     <>
-      <div className=""></div>
+      <div className="min-w-screen flex justify-center mt-10">Please Login</div>
     </>
   );
+};
+
+const User = ({ session }) => {
+  return (
+    <>
+      <div className="min-w-screen flex justify-center mt-10">Welcome</div>
+    </>
+  );
+};
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  return <>{session ? User({ session }) : Guest()}</>;
 }
