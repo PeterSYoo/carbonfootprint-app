2d583d4c16d6dff742a6bcbb44a1299f85133ea9
Peter Yoo <trustintofu@gmail.com>
2022-11-16 17:13:55 -0800

    feat: Mongoose schema for users.

---
+++ model/Users.js
@@ -0,0 +1,15 @@
+import { Schema, model, models } from 'mongoose';
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  name: String,
+  email: String,
+  image: String,
+  password: String,
+  cityState: String,
+});
+
+const Users = models.user || model('user', userSchema);
+
+export default Users;
