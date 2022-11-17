7c14e9c71515957e7f9e710882f396b3d6fbfe59
Peter Yoo <trustintofu@gmail.com>
2022-11-16 19:04:18 -0800

    feat: Next auth api endpoint.

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../lib/mongoAuthAdapter';
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async session({ session, user }) {
+      session.id = user.id;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
