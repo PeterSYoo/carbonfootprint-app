e200bcae2e226c7cead6e02f007084510f40871e
Peter Yoo <trustintofu@gmail.com>
2022-11-17 19:00:32 -0800

    feat: Imports loading spinner, loads on conditional render.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,10 +7,35 @@ import {
   QueryClient,
   Hydrate,
 } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import { LoadingSpinner } from '../components/LoadingSpinner.components';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
@@ -18,7 +43,15 @@ function MyApp({ Component, pageProps }) {
           <SessionProvider session={pageProps.session}>
             <ThemeProvider enableSystem={true} attribute="class">
               <Header />
-              <Component {...pageProps} />
+              {loading ? (
+                <>
+                  <LoadingSpinner />
+                </>
+              ) : (
+                <>
+                  <Component {...pageProps} />
+                </>
+              )}
             </ThemeProvider>
           </SessionProvider>
         </Hydrate>
