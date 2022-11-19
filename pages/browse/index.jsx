8c074c3236472fc07ee777c55c5e93bf92fe9960
Peter Yoo <trustintofu@gmail.com>
2022-11-19 11:55:47 -0800

    feat: Creates browse page, maps fetched clothes data.

--- pages/browse/index.jsx
+++ pages/browse/index.jsx
@@ -1,5 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { Fragment } from 'react';
+import { LoadingSpinner } from '../../components/LoadingSpinner.components';
+import { getClothes } from '../../lib/clothesHelper';
+
 const BrowsePage = () => {
-  return <div>BrowsePage</div>;
+  const { data, isLoading, isError, error } = useQuery(['clothes'], () =>
+    getClothes()
+  );
+
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return { error };
+
+  return (
+    <>
+      <div className="w-5/6 grid grid-cols-2 mx-auto mt-8 gap-8">
+        {data?.map((clothes) => (
+          <Fragment key={data?._id}>
+            <div className="flex flex-col w-full">
+              <div className="bg-gray-300 h-36"></div>
+              <div className="flex justify-center">{clothes.name}</div>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default BrowsePage;
