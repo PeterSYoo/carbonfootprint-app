3afbdd8d5f600999a7aab7441f04778699fc3843
Peter Yoo <trustintofu@gmail.com>
2022-11-18 18:57:02 -0800

    feat: Removes login, rearranges title.

--- components/Header.components.jsx
+++ components/Header.components.jsx
@@ -3,42 +3,18 @@ import Link from 'next/link';
 import React from 'react';
 import { ThemeButton } from './ThemeButton.components';
 
-const Guest = () => {
-  return (
-    <>
-      <div className="flex justify-between mt-6 mx-auto px-10 md:max-w-[1024px]">
-        <Link href="/">
-          <button className="text-xl font-bold">Logo</button>
-        </Link>
-        <div className="flex items-center gap-10">
-          <ThemeButton />
-          <Link href="/login">
-            <button>Login</button>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
-};
-
-const User = () => {
-  return (
-    <>
-      <div className="flex justify-between mt-6 mx-auto px-10 md:max-w-[1024px]">
-        <Link href="/">
-          <button className="text-xl font-bold">Logo</button>
-        </Link>
-        <div className="flex items-center gap-10">
-          <ThemeButton />
-          <button onClick={() => signOut()}>Logout</button>
-        </div>
-      </div>
-    </>
-  );
-};
-
 export const Header = () => {
-  const { data: session } = useSession();
-
-  return <>{session ? User({ session }) : Guest()}</>;
+  return (
+    <>
+      <div className="flex flex-col bg-gray-300 pt-16 items-center">
+        <div className="mb-2">
+          <ThemeButton />
+        </div>
+        <Link href="/">
+          <h1 className="text-3xl font-bold mb-3">Our Closet</h1>
+        </Link>
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+    </>
+  );
 };
