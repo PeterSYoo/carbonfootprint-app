01e6747f960696319f2dfa2b67f63877c4811e2c
Peter Yoo <trustintofu@gmail.com>
2022-11-18 18:58:57 -0800

    feat: Creates landing page for guest. Forwards user to change username page after logging in.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,28 @@
 import { useSession } from 'next-auth/react';
+import { FcGoogle } from 'react-icons/fc';
+import { signIn } from 'next-auth/react';
 
-const Guest = () => {
+const Guest = (handleGoogleSignin) => {
   return (
     <>
-      <div className="min-w-screen flex justify-center mt-10">Please Login</div>
+      <div className="min-w-screen flex flex-col mt-8">
+        <div className="bg-gray-300 w-5/6 h-48 flex justify-center items-center mx-auto">
+          Sustainable Clothing Rental Community
+        </div>
+        <div className="mt-10 bg-gray-300 w-5/6 h-[400px] mx-auto flex justify-center items-center">
+          Hero Image
+        </div>
+        <div className="mt-10 mx-auto text-xl font-bold">
+          Sign in with Google
+        </div>
+        <button
+          type="button"
+          onClick={handleGoogleSignin}
+          className="mt-8 mx-auto bg-gray-300 px-6 py-2 rounded-xl"
+        >
+          <FcGoogle size={35} />
+        </button>
+      </div>
     </>
   );
 };
@@ -19,5 +38,11 @@ const User = ({ session }) => {
 export default function Home() {
   const { data: session } = useSession();
 
-  return <>{session ? User({ session }) : Guest()}</>;
+  const handleGoogleSignin = async () => {
+    signIn('google', {
+      callbackUrl: '/change-username',
+    });
+  };
+
+  return <>{session ? User({ session }) : Guest(handleGoogleSignin)}</>;
 }
