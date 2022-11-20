977fb78e6d0a17e391a7f8f084cb40c8a42d2f32
stevencueto <steven.cueto1305@gmail.com>
2022-11-20 13:29:25 -0500

    feat: messages api endpoint created

---
+++ pages/api/messages/index.js
@@ -0,0 +1,29 @@
+import usersConnect from "../../../lib/usersConnect";
+import {getMessages,  createMessage, putMessages} from "../../../lib/messagesController";
+
+export default async function usersApi(req, res) {
+  usersConnect().catch(() => {
+    res.status(405).json({ error: 'Error in connecting to users databse' });
+  });
+
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      getMessages(req, res);
+      break;
+    case 'PUT':
+      putMessages(req, res);
+      break;
+    case 'POST':
+      createMessage(req, res);
+      break;
+    case 'DELETE':
+      deleteMessages(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', "PUT", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
