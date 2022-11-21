64d54a39dd0829157803a1526a40ccc0e9c779e1
stevencueto <steven.cueto1305@gmail.com>
2022-11-21 11:44:33 -0500

    refactor: fixed bugs in the selectsize component

--- components/SelectSize.component.jsx
+++ components/SelectSize.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SelectSize = ({handleChange, newItem}) => {
   return (
-    <select name="size" value={sortValue} onChange={(e)=> handleChange(e)}>
+    <select name="size" value={newItem.size} onChange={(e)=> handleChange(e)}>
       <option value="XXS">XXS</option>
       <option value="XS">xs</option>
       <option value="S">Category</option>
