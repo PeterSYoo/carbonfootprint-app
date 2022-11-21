1b493b53e3a10bc625b6bb19cd4a36194a59333f
stevencueto <steven.cueto1305@gmail.com>
2022-11-20 21:45:03 -0500

    feat: created find and filter form  in a new page

---
+++ pages/test/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SearchMenu } from '../../components/SearchMenu.component'
+
+export default function Test(){
+  return (
+    <div>
+      <SearchMenu/>
+    </div>
+  )
+}
