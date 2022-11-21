84e504be4c5e68fba8f08280762dc37a2b3588d8
Peter Yoo <trustintofu@gmail.com>
2022-11-20 18:10:16 -0800

    feat: Changes bg color and adds links to icons.

--- components/Footer.components.jsx
+++ components/Footer.components.jsx
@@ -1,7 +1,8 @@
 import { RiSettings5Line } from 'react-icons/ri';
-import { BsChatDots, BsSearch } from 'react-icons/bs';
+import { BsChatDots, BsSearch, BsPlusCircle } from 'react-icons/bs';
 import { FiUser } from 'react-icons/fi';
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const Guest = () => {
   return <></>;
@@ -11,11 +12,15 @@ const User = () => {
   return (
     <>
       <div className="fixed inset-x-0 bottom-0">
-        <div className="flex justify-between w-full bg-gray-300 px-10 py-4">
-          <RiSettings5Line size={28} />
-          <BsChatDots size={28} />
-          <BsSearch size={28} />
-          <FiUser size={28} />
+        <div className="flex justify-between w-full bg-teal px-10 py-4 text-white">
+          <BsPlusCircle size={28} />
+          <BsChatDots size={28} className="opacity-30" />
+          <Link href="/browse">
+            <BsSearch size={28} />
+          </Link>
+          <Link href="/profile">
+            <FiUser size={28} />
+          </Link>
         </div>
       </div>
     </>
