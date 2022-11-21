89dc23cc751d48ace695229ff88a5c37aeb551bc
Peter Yoo <trustintofu@gmail.com>
2022-11-20 18:36:59 -0800

    feat: Adds logout icon and functionality.

--- components/Footer.components.jsx
+++ components/Footer.components.jsx
@@ -1,26 +1,29 @@
-import { RiSettings5Line } from 'react-icons/ri';
+import { FiLogOut } from 'react-icons/fi';
 import { BsChatDots, BsSearch, BsPlusCircle } from 'react-icons/bs';
 import { FiUser } from 'react-icons/fi';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Guest = () => {
   return <></>;
 };
 
-const User = () => {
+const User = (handleSignout) => {
   return (
     <>
       <div className="fixed inset-x-0 bottom-0">
         <div className="flex justify-between w-full bg-teal px-10 py-4 text-white">
-          <BsPlusCircle size={28} />
+          <Link href="/profile">
+            <FiUser size={28} />
+          </Link>
           <BsChatDots size={28} className="opacity-30" />
           <Link href="/browse">
             <BsSearch size={28} />
           </Link>
-          <Link href="/profile">
-            <FiUser size={28} />
-          </Link>
+          <button onClick={() => handleSignout()}>
+            <FiLogOut size={28} />
+          </button>
         </div>
       </div>
     </>
@@ -28,7 +31,13 @@ const User = () => {
 };
 
 export const Footer = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
-  return <>{session ? User() : Guest()}</>;
+  const handleSignout = async () => {
+    await signOut();
+    router.push('/');
+  };
+
+  return <>{session ? User(handleSignout) : Guest()}</>;
 };
